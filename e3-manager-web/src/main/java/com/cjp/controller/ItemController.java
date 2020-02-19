e897a8aabd2ce2c56f19b8daf9084d0ad7e15c6f
cjp <2462023146@qq.com>
2020-02-19 12:36:35 +0800

    面向服务架构

---
+++ e3-manager-web/src/main/java/com/cjp/controller/ItemController.java
@@ -0,0 +1,23 @@
+package com.cjp.controller;
+
+
+import com.e3mall.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pojo.TbItem;
+
+@Controller
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping("/item/{id}")
+    @ResponseBody
+    private TbItem getItemById(@PathVariable Long id) {
+        TbItem tbItem = itemService.getItemById(id);
+        return tbItem;
+    }
+}
