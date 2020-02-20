b597283948e23ddb535d3e9bd06621bb4e5e8c3b
cjp <2462023146@qq.com>
2020-02-20 10:23:25 +0800

    面向服务架构

--- e3-manager-web/src/main/java/com/cjp/controller/ItemController.java
+++ e3-manager-web/src/main/java/com/cjp/controller/ItemController.java
@@ -2,6 +2,7 @@ package com.cjp.controller;
 
 
 import com.e3mall.service.ItemService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
